0c0220a5998911b1938f1b20b512144cd1db3b5b
Chrissy Wainwright <chrissy@sixfeetup.com>
2017-08-15 10:44:51 -0500

    PEP8 Fixes

--- src/collective/emergency/alerts/browser/alert.py
+++ src/collective/emergency/alerts/browser/alert.py
@@ -3,22 +3,23 @@ from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from zope.component import getUtility
 from plone.registry.interfaces import IRegistry
-import datetime,time,hashlib,json,md5
+import datetime, time, hashlib, json
+
 
 class Alert(object):
 
     id = None
     _struct = {}
-    
+
     def __init__(self, id):
         self._struct = {
-            u'title' : u'',
-            u'body' : u'',
-            u'start' : u'',
-            u'end' : u'',
-            u'level' : u'0',
-            u'is_active' : u'False',
-            u'url' : u'',
+            u'title': u'',
+            u'body': u'',
+            u'start': u'',
+            u'end': u'',
+            u'level': u'0',
+            u'is_active': u'False',
+            u'url': u'',
         }
         if id:
             registry = getUtility(IRegistry)
@@ -28,156 +29,149 @@ class Alert(object):
                 self._struct = alerts[self.id]
         else:
             self.id = hashlib.sha1(str(time.time())).hexdigest().decode('utf-8')
-            
+
     def save(self):
         registry = getUtility(IRegistry)
         alerts = registry['collective.emergency.alerts.browser.controlpanel.IEmergencyAlert.alerts']
         if not alerts:
             alerts = {}
-        alerts[self.id] = self._struct 
+        alerts[self.id] = self._struct
         registry['collective.emergency.alerts.browser.controlpanel.IEmergencyAlert.alerts'] = alerts
-        
+
         # Force the save of a dictionary to be persistant
         registry['collective.emergency.alerts.browser.controlpanel.IEmergencyAlert.alerts'] = registry['collective.emergency.alerts.browser.controlpanel.IEmergencyAlert.alerts']
-            
+
     def delete(self):
         registry = getUtility(IRegistry)
         alerts = registry['collective.emergency.alerts.browser.controlpanel.IEmergencyAlert.alerts']
         del alerts[self.id]
         registry['collective.emergency.alerts.browser.controlpanel.IEmergencyAlert.alerts'] = alerts
-        
+
         # Force the save of a dictionary to be persistant
         registry['collective.emergency.alerts.browser.controlpanel.IEmergencyAlert.alerts'] = registry['collective.emergency.alerts.browser.controlpanel.IEmergencyAlert.alerts']
-           
-            
+
     def set(self, name, value):
         if isinstance(value, str):
             value = value.decode('utf-8')
         self._struct[name] = value
-            
+
     def get(self, name):
         return self._struct[name]
-        
-    # def _reset(self):
-        # registry = getUtility(IRegistry)
-        # alerts = registry['collective.emergency.alerts.browser.controlpanel.IEmergencyAlert.alerts']
-        # if not alerts:
-            # registry['collective.emergency.alerts.browser.controlpanel.IEmergencyAlert.alerts'] = {}
-            # # Force the save of a dictionary to be persistant
-            # registry['collective.emergency.alerts.browser.controlpanel.IEmergencyAlert.alerts'] = registry['collective.emergency.alerts.browser.controlpanel.IEmergencyAlert.alerts']
-         
 
-        
-        
+    # def _reset(self):
+    #     registry = getUtility(IRegistry)
+    #     alerts = registry['collective.emergency.alerts.browser.controlpanel.IEmergencyAlert.alerts']
+    #     if not alerts:
+    #         registry['collective.emergency.alerts.browser.controlpanel.IEmergencyAlert.alerts'] = {}
+    #         # Force the save of a dictionary to be persistant
+    #         registry['collective.emergency.alerts.browser.controlpanel.IEmergencyAlert.alerts'] = registry['collective.emergency.alerts.browser.controlpanel.IEmergencyAlert.alerts']
+
+
 class AlertEdit(BrowserView):
 
     template = ViewPageTemplateFile('edit_alert.pt')
     alert = None
     error = False
-   
+
     def __call__(self):
-        self.alert = None #default
-        self.error = None #default
+        self.alert = None  # default
+        self.error = None  # default
         id = self.request.form.get('id', None)
-        
+
         try:
             self.alert = Alert(id)
         except Exception as e:
             self.error = True
-            
+
         if 'form.widgets.submit' in self.request.form:
             self.alert.set(u'title', self.request.form.get('form.widgets.title', 'Missing title'))
             self.alert.set(u'body', self.request.form.get('form.widgets.body', 'No details available at this time'))
-            
+
             self.alert.set(u'start', self.request.form.get('form.widgets.start', ''))
             self.alert.set(u'end', self.request.form.get('form.widgets.end', ''))
             self.alert.set(u'url', self.portal.absolute_url() + '/alert?id=' + str(self.alert.id))
-            
+
             active = u'False'
             if self.request.form.get('form.widgets.active', 'off') == 'on':
                 active = u'True'
             self.alert.set(u'is_active', active)
-            
+
             self.alert.set(u'level', self.request.form.get('form.widgets.level', '0'))
-            
+
             self.alert.save()
             return self.request.response.redirect(self.portal.absolute_url() + '/@@emergency_manager')
-            
+
         if 'alert.state' in self.request.form:
             state = self.request.form.get('alert.state')
             if state == 'False':
                 self.alert.set('is_active', u'True')
-            else: 
+            else:
                 self.alert.set('is_active', u'False')
             self.alert.save()
             return self.request.response.redirect(self.portal.absolute_url() + '/@@emergency_manager')
-            
+
         if 'alert.remove' in self.request.form:
             self.alert.delete()
             return self.request.response.redirect(self.portal.absolute_url() + '/@@emergency_manager')
-            
+
         return self.template()
-        
+
     @property
     def portal(self):
         return api.portal.get()
-        
-        
+
+
 class AlertView(BrowserView):
 
     template = ViewPageTemplateFile('alert.pt')
     alert = None
-    
+
     def __call__(self):
-    
-        self.alert = None #default
-        self.error = None #default
+
+        self.alert = None  # default
+        self.error = None  # default
         id = self.request.form.get('id', None)
-        
+
         try:
             self.alert = Alert(id)
         except Exception as e:
             self.error = True
-            
+
         return self.template()
-        
-        
+
+
 class AlertsBroadcaster(BrowserView):
-    
+
     def __call__(self):
         data = []
         now = datetime.datetime.now()
-        
+
         registry = getUtility(IRegistry)
         alerts = registry['collective.emergency.alerts.browser.controlpanel.IEmergencyAlert.alerts']
         if alerts:
-            for k,v in alerts.items():
+            for k, v in alerts.items():
                 if v['is_active'] == 'True':
                     start = datetime.datetime.strptime(self.dict_get(v, 'start', '1999-12-25 13:12'), '%Y-%m-%d %H:%M')
                     end = datetime.datetime.strptime(self.dict_get(v, 'end', '2050-12-25 13:12'), '%Y-%m-%d %H:%M')
                     if start <= now and now <= end:
                         data.append(v)
-                        
+
         # Determine Format
-        #self.request.response.setHeader('ETag', md5.new(str(data)).hexdigest())
-        #self.request.response.setHeader('Cache-Control', 'max-age=60, s-maxage=60, public, must-revalidate')
-        #self.request.response.setHeader('Vary', 'Accept-Encoding')
+        # self.request.response.setHeader('ETag', md5.new(str(data)).hexdigest())
+        # self.request.response.setHeader('Cache-Control', 'max-age=60, s-maxage=60, public, must-revalidate')
+        # self.request.response.setHeader('Vary', 'Accept-Encoding')
         self.request.response.setHeader('Content-Type', 'application/json')
         self.request.response.setHeader('Access-Control-Allow-Origin', '*')
-        return self.toJSONP(data)
-        
-    def dict_get(self,v,key,default):
+        return self.toJSON(data)
+
+    def dict_get(self, v, key, default):
         if key in v:
             if not v[key]:
                 return default
             return v[key]
-            
+
     def toJSON(self, data):
         return json.dumps(data)
-        
+
     def toJSONP(self, data):
         return '_EAS.loaded(' + self.toJSON(data) + ')'
-        
-
-        
-        
--- src/collective/emergency/alerts/browser/eas_generator.py
+++ src/collective/emergency/alerts/browser/eas_generator.py
@@ -3,12 +3,12 @@ from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from zope.component import getUtility
 from plone.registry.interfaces import IRegistry
-import datetime,time,hashlib,json
+
 
 class EASGenerator(BrowserView):
 
     template = ViewPageTemplateFile('eas.pt')
-   
+
     def __call__(self):
         self.request.response.setHeader('Content-Type', 'application/javascript')
         self.request.response.setHeader('X-Theme-Disabled', '1')
@@ -16,21 +16,19 @@ class EASGenerator(BrowserView):
         self.request.response.setHeader('Vary', 'Accept-Encoding')
         self.request.response.setHeader('Access-Control-Allow-Origin', '*')
         return self.template()
-        
-        
+
     def get_feeds(self):
         registry = getUtility(IRegistry)
-        
+
         # add global feeds, use list to clone it otherwise it saves?!
         feeds = list(registry['collective.emergency.alerts.browser.controlpanel.IEmergencyAlert.global_feeds']) 
         if not feeds:
             feeds = []
         else:
             feeds = [x.encode('ascii', 'ignore') for x in feeds]
-        feeds.append(self.portal.absolute_url() + '/eas_alerts') # add local feed
+        feeds.append(self.portal.absolute_url() + '/eas_alerts')  # add local feed
         return feeds
-        
-        
+
     @property
     def portal(self):
         return api.portal.get()
